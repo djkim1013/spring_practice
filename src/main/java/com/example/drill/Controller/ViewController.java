5195eeeeece48335061f670745dcbb6202eb47e7
djkim <dongjukim1013@gmail.com>
2022-05-16 11:51:46 +0900

    Revert "config"
    This reverts commit 21b13ae85a4e11c2e095c8ffaf9fc74b17691bf5.

--- src/main/java/com/example/drill/Controller/ViewController.java
+++ src/main/java/com/example/drill/Controller/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class IndexController {
+public class ViewController {
 
     @GetMapping("favicon.ico")
     @ResponseBody
