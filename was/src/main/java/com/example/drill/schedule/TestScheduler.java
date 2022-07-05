df240e8bfaa79857c5fecc0ae0835d7992514186
djkim <dongjukim1013@gmail.com>
2022-07-05 11:54:41 +0900

    schedule period

--- was/src/main/java/com/example/drill/schedule/TestScheduler.java
+++ was/src/main/java/com/example/drill/schedule/TestScheduler.java
@@ -3,6 +3,7 @@ package com.example.drill.schedule;
 import com.example.drill.config.Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
+@RefreshScope
 public class TestScheduler {
 
     @Scheduled(cron = "${drill.test-schedule-period}")
