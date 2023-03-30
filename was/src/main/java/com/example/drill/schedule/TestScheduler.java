b3f17edcdd725001fca94b07cd13e2fd61a96363
djkim <dongjukim1013@gmail.com>
2023-03-30 18:00:38 +0900

    230330

--- was/src/main/java/com/example/drill/schedule/TestScheduler.java
+++ was/src/main/java/com/example/drill/schedule/TestScheduler.java
@@ -1,8 +1,6 @@
 package com.example.drill.schedule;
 
-import com.example.drill.config.Config;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,12 +8,13 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Slf4j
 @Component
 @RefreshScope
 public class TestScheduler {
 
     @Scheduled(cron = "${drill.test-schedule-period}")
     public void scheduleHang(){
-        System.out.println("hang : " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        log.info("hang : " + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
     }
 }
