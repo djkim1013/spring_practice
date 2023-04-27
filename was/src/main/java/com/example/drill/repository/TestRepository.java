cef31b21c5f49a034ba3b4f3b0ebb2e75a8199e3
djkim <dongjukim1013@gmail.com>
2023-04-27 18:03:40 +0900

    transactional test

--- was/src/main/java/com/example/drill/repository/TestRepository.java
+++ was/src/main/java/com/example/drill/repository/TestRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TestRepository extends JpaRepository<User,Long> {
+public interface TestRepository extends JpaRepository<User,Integer> {
     List<User> findAll();
 
     User findByUserId(int userId);
--- was/src/main/java/com/example/drill/service/TestService.java
+++ was/src/main/java/com/example/drill/service/TestService.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
 public class TestService {
@@ -31,6 +34,7 @@ public class TestService {
         testRepository.save(new User(String.format("User%03d", i + 1)));
     }
 
+    @Transactional
     public void putUsers(Integer n) {
         for (int i = 0; i < n; i++) {
             service.putUser(i);
@@ -39,6 +43,7 @@ public class TestService {
 
     @Transactional
     protected void putUser(int i) {
-        testRepository.findByUserId(i);
+        User user = testRepository.findById(i + 1).orElseThrow(() -> new RuntimeException("not found"));
+        user.setUserName(user.getUserName() + "mod" + LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
