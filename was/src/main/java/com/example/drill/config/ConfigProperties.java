e26662df5a039c6481d542fcf2e880ef26156a1a
djkim <dongjukim1013@gmail.com>
2023-04-28 17:46:04 +0900

    security

--- was/src/main/java/com/example/drill/config/ConfigProperties.java
+++ was/src/main/java/com/example/drill/config/ConfigProperties.java
@@ -1,14 +1,15 @@
 package com.example.drill.config;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @RefreshScope
 @Component
-@Getter
-public class Config {
-//    @Value("${drill.value}")
-    String value;
+@Data
+public class ConfigProperties {
+
+    @Value("${drill.value:-}")
+    public String value;
 }
