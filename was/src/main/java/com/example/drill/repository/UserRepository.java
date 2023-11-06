afad3cad3e56650e81b3154cbac36b6cb1609eb9
djkim <dongjukim1013@gmail.com>
2023-11-06 11:47:03 +0900

    save

--- was/src/main/java/com/example/drill/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.drill.repository;
-
-import com.example.drill.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- was/src/main/java/com/example/drill/service/TestService.java
+++ was/src/main/java/com/example/drill/service/TestService.java
@@ -1,38 +1,35 @@
 package com.example.drill.service;
 
-import com.example.drill.domain.entity.MainProductRedisHash;
-import com.example.drill.repository.ProductRedisRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TestService {
 
-//    private final RedisTemplate<String, MainProductRedisHash> redisTemplate;
-    private final ProductRedisRepository repository;
+    private final RedisTemplate<String, String> redisTemplate;
+//    private final ProductRedisRepository repository;
 
+    @Scheduled(initialDelay = 10L, fixedRate = 10000L)
     public void testTemplate(){
         log.info("start timer");
         LocalDateTime startTime = LocalDateTime.now();
-//        Set<String> keySet = redisTemplate.keys("mainProducts");
-//        List<MainProductRedisHash> productsList = redisTemplate.opsForValue().multiGet(keySet);
-//        log.info("redistemplate findall : {}", productsList);
+        List<Object> getResultMap = redisTemplate.opsForHash().multiGet("mainProducts", List.of("mainProductId", "productName"));
+        log.info("redistemplate findall : {}", getResultMap);
         log.info("elapsed time :: {}", Duration.between(startTime, LocalDateTime.now()).toMillis());
-        log.info("reset timer");
-        startTime = LocalDateTime.now();
-        List<MainProductRedisHash> productsList = (List<MainProductRedisHash>) repository.findAll();
-        log.info("crud repository findall : {}", productsList);
-        log.info("elapsed time :: {}", Duration.between(startTime, LocalDateTime.now()).toMillis());
+
+//        log.info("reset timer");
+//        startTime = LocalDateTime.now();
+//        List<MainProductRedisHash> productsList = (List<MainProductRedisHash>) repository.findAll();
+//        log.info("crud repository findall : {}", productsList);
+//        log.info("elapsed time :: {}", Duration.between(startTime, LocalDateTime.now()).toMillis());
     }
 }
