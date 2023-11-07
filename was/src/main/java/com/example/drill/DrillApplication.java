c689db031c1970b85b7beac880fd57046013ae6b
djkim <dongjukim1013@gmail.com>
2023-11-07 18:09:06 +0900

    save

--- was/src/main/java/com/example/drill/DrillApplication.java
+++ was/src/main/java/com/example/drill/DrillApplication.java
@@ -1,14 +1,14 @@
 package com.example.drill;
 
+import com.redis.om.spring.annotations.EnableRedisEnhancedRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling
-@EnableRedisRepositories(basePackages = "com.example.drill.repository")
+@EnableRedisEnhancedRepositories(basePackages = "com.example.drill.repository")
 public class DrillApplication {
 
     public static void main(String[] args) {
